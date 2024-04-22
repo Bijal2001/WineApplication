ff9c6f460f52f09e4a7daacf826a454fd7ccf745
pavanantala <pavanantala7@gmail.com>
2024-04-22 19:14:01 -0400

    Read Database

---
+++ app/src/main/java/com/example/wineapplication/constant/DBConstant.java
@@ -0,0 +1,16 @@
+package com.example.wineapplication.constant;
+
+
+/**
+ * Constants related to database file
+ * Such as file path and file name
+ */
+public abstract class DBConstant
+{
+    //database file directory
+    public static String DATABASE_PATH = "/data/data/com.example.wineapplication/databases";
+    //database file name
+    public static String DATABASE_FILE = "WineApplication.db";
+    //database version
+    public static int DATABASE_VERSION = 1;
+}
---
+++ app/src/main/java/com/example/wineapplication/constant/SQLCommand.java
@@ -0,0 +1,14 @@
+package com.example.wineapplication.constant;
+
+/**
+ * SQL commands
+ * Including select/delete/update/insert
+ */
+
+public abstract class SQLCommand
+{
+    //query all students
+    public static String QUERY_STUDENT = "select stid, stname from Student";
+}
+
+

e46fb5d99d3ae582e9f20cdb0728da46380ac103
Bijal2001 <89074841+Bijal2001@users.noreply.github.com>
2024-04-27 22:13:49 -0400

    Bijal_work

---
+++ app/src/main/java/com/example/wineapplication/DetailActivity.java
@@ -0,0 +1,29 @@
+package com.example.wineapplication;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.bumptech.glide.Glide;
+import com.example.wineapplication.databinding.ActivityDetailBinding;
+
+public class DetailActivity extends AppCompatActivity {
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_detail);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+}
